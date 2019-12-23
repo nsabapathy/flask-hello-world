21fe935cc8ed2b044d592a5f1705dd700454465d
Naveen Sabapathy <naveen.sabapathy@Naveens-MacBook-Pro.local>
2019-12-22 23:11:18 -0800

    added home.html template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, escape, url_for
+from flask import Flask, escape, url_for, render_template
 app = Flask(__name__)
 
 #Modified by naveen
 @app.route('/')
 def hello_world():
-    return 'This is test of Render service!'
+    return render_template('home.html', message='This is a hello')
 
 
 @app.route('/info')

d487fab53293d3e6d338e9c50f5367187f00d3ba
Naveen Sabapathy <naveen.sabapathy@Naveens-MacBook-Pro.local>
2019-12-22 22:54:38 -0800

    added info route

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, escape, url_for
 app = Flask(__name__)
 
 #Modified by naveen
 @app.route('/')
 def hello_world():
     return 'This is test of Render service!'
+
+
+@app.route('/info')
+def info():
+    return ' This is the info page'
+
+with app.test_request_context():
+    print(url_for('info'))

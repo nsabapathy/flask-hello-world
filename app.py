e997203916a6b7d47e136e6bdfb277162335a273
Naveen Sabapathy <naveen.sabapathy@Naveens-MacBook-Pro.local>
2019-12-27 17:11:22 -0800

    new layout, image updates etc

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask, escape, url_for, render_template
-
+import glob
 
 app = Flask(__name__)
 
 #Modified by naveen
 @app.route('/')
 def hello_world():
-    return render_template('home3.html', message='This is a hello')
+    #create a list of dogs/cats images 
+    print("In hello world")
+    img_list = glob.glob("static/img/*.jpg")[0:10]
+    print(img_list)
+    return render_template('home3.html', img_list = img_list)
 
 
 @app.route('/info')
 def info():
-    return ' This is the info page'
+    return 'This is the info page'
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+   return 'From Analyze function'
+
+
+@app.route('/load_image', methods=['POST'])
+def load_image(): 
+   print("In load image method in server ")
+   return 'Image-Id'
 
 with app.test_request_context():
     print(url_for('info'))
